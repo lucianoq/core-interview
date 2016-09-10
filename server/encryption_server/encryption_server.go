f4386dac505ac87ad950ced4684aa12d7f8f3135
Luciano Quercia <lquercia@it.ibm.com>
2016-09-10 17:47:38 +0200

    Encryption skeleton

--- server/encryption_server/encryption_server.go
+++ server/encryption_server/encryption_server.go
@@ -1,12 +1,36 @@
 package server
 
-func Store(id, payload []byte) (aesKey []byte, err error) {
-	//TODO
-	return []byte("this is a key"), nil
+import "core-interview/server/storage"
+
+func Store(id, payload []byte) ([]byte, error) {
+	aesKey := generateAesKey()
+	cyphertext := encrypt(payload, aesKey)
+	err := storage.Store(id, cyphertext)
+	if err != nil {
+		return nil, err
+	}
+	return aesKey, nil
 }
 
-func Retrieve(id, aesKey []byte) (payload []byte, err error) {
-	//TODO
-	return []byte("original text"), nil
+func Retrieve(id, aesKey []byte) ([]byte, error) {
+	cyphertext, err := storage.Retrieve(id)
+	if err != nil {
+		return nil, err
+	}
+	return decrypt(cyphertext, aesKey), nil
 }
 
+func encrypt(msg, key []byte) []byte {
+	//TODO
+	return msg
+}
+
+func decrypt(crypt, key []byte) []byte {
+	//TODO
+	return crypt
+}
+
+func generateAesKey() []byte {
+	//TODO random
+	return []byte("example")
+}
