97171f0833a0c9e407d128d575a580059b1cf39e
Luciano Quercia <lquercia@it.ibm.com>
2016-09-10 19:36:38 +0200

    Both routes want POST with JSON

--- server/webserver/webserver.go
+++ server/webserver/webserver.go
@@ -6,44 +6,80 @@ import (
 	"net/http"
 	es "core-interview/server/encryption_server"
 	"fmt"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func Start() {
 	rtr := mux.NewRouter()
 
 	rtr.HandleFunc("/store", store).Methods("POST")
-	rtr.HandleFunc("/retrieve/{id}/{key}", retrieve).Methods("GET")
+	rtr.HandleFunc("/retrieve", retrieve).Methods("POST")
 
 	http.Handle("/", rtr)
-	
+
 	log.Print("Server starting...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func store(w http.ResponseWriter, r *http.Request) {
 	log.Print("Received POST on /store")
-	vars := mux.Vars(r)
-	id, data := vars["id"], vars["data"]
-	log.Printf("--- from id=`%s`", id)
-	aesKey, err := es.Store([]byte(id), []byte(data))
 
+	type StoreRequest struct {
+		Id   string `json:"id"`
+		Data string `json:"data"`
+	}
+
+	body, err := ioutil.ReadAll(r.Body);
 	if err != nil {
+		log.Print(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var req StoreRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		log.Print(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Printf("--- from id=`%s`", req.Id)
+
+	aesKey, err := es.Store(req.Id, req.Data)
+	if err != nil {
+		log.Print(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	fmt.Fprintf(w, "%s", string(aesKey))
+	fmt.Fprintf(w, "%s", aesKey)
 }
 
 func retrieve(w http.ResponseWriter, r *http.Request) {
 	log.Print("Received GET on /retrieve")
-	vars := mux.Vars(r)
-	id, key := vars["id"], vars["key"]
-	log.Printf("--- with id=`%s` and key=`%s`", id, key)
-	payload, err := es.Retrieve([]byte(id), []byte(key))
+
+	type RetrieveRequest struct {
+		Id  string `json:"id"`
+		Key string `json:"key"`
+	}
+	body, err := ioutil.ReadAll(r.Body);
+	if err != nil {
+		log.Print(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var req RetrieveRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		log.Print(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("--- with id=`%s`", req.Id)
+	payload, err := es.Retrieve(req.Id, req.Key)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "%s", string(payload))
+	fmt.Fprintf(w, "%s", payload)
 }
